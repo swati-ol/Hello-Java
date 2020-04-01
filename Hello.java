507abe1136945ada2715678d3d641977fbad7fd5
swati-ol <62972802+swati-ol@users.noreply.github.com>
2020-04-01 13:33:43 +1100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public class Hello {
-public static void main(String[] args) {
+public class Hello{
+  public static void main(String[] args){
 // Prints "Hello, World" in the terminal window.
-system.out.println("Hello, World.");
+System.out.println("Hello, World.");
 }
 }
